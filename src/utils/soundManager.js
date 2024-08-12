7878a268f71999d7de8adab03f38dd38d3bc9e6d
George Dixon <george__dixon@outlook.com>
2024-08-12 09:33:50 +0100

    lint

--- src/utils/soundManager.js
+++ src/utils/soundManager.js
@@ -4,43 +4,43 @@ import { Howl } from "howler";
  * SoundManager class
  */
 class SoundManager {
-  constructor() {
-    this._sounds = {};
-  }
+	constructor() {
+		this._sounds = {};
+	}
 
-  /**
-   * Load a sound asset
-   *
-   * @param {string} key - the key to reference the sound
-   * @param {string} src - the source file path of the sound
-   */
-  loadSound(key, src) {
-    this._sounds[key] = new Howl({ src: [src], format: "wav" });
-  }
+	/**
+	 * Load a sound asset
+	 *
+	 * @param {string} key - the key to reference the sound
+	 * @param {string} src - the source file path of the sound
+	 */
+	loadSound(key, src) {
+		this._sounds[key] = new Howl({ src: [src], format: "wav" });
+	}
 
-  /**
-   * Play a sound by its key
-   *
-   * @param {string} key - the key of the sound to play
-   */
-  playSound(key) {
-    const sound = this._sounds[key];
-    if (sound) {
-      sound.play();
-    }
-  }
+	/**
+	 * Play a sound by its key
+	 *
+	 * @param {string} key - the key of the sound to play
+	 */
+	playSound(key) {
+		const sound = this._sounds[key];
+		if (sound) {
+			sound.play();
+		}
+	}
 
-  /**
-   * Stop a sound by its key
-   *
-   * @param {string} key - the key of the sound to stop
-   */
-  stopSound(key) {
-    const sound = this._sounds[key];
-    if (sound) {
-      sound.stop();
-    }
-  }
+	/**
+	 * Stop a sound by its key
+	 *
+	 * @param {string} key - the key of the sound to stop
+	 */
+	stopSound(key) {
+		const sound = this._sounds[key];
+		if (sound) {
+			sound.stop();
+		}
+	}
 }
 
 export const soundManager = new SoundManager();
