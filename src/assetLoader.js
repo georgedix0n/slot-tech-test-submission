dbf55cac5baedcd9a909c8a83b9582d3279cfa38
George Dixon <george__dixon@outlook.com>
2024-08-12 09:35:44 +0100

    Altered asset loader to load sounds with sound manager

--- src/assetLoader.js
+++ src/assetLoader.js
@@ -1,31 +1,52 @@
 import * as PIXI from "pixi.js";
+import { soundManager } from "./utils/soundManager.js";
 
 /**
  *
  */
 class AssetLoader {
-	constructor() {
-		this._queue = [];
-	}
+  constructor() {
+    this._queue = [];
+    this._soundQueue = {};
+  }
 
-	/**
-	 * add an asset to the queue of assets to load using PIXI
-	 *
-	 * @param {*} asset - any asset we need to load
-	 */
-	addToQueue(asset) {
-		this._queue.push(asset);
-	}
+  /**
+   * add an asset to the queue of assets to load using PIXI
+   *
+   * @param {*} asset - any asset we need to load
+   */
+  addToQueue(asset) {
+    this._queue.push(asset);
+  }
 
-	/**
-	 * lead all assets added to the queue and reset the to the queue
-	 *
-	 * @async
-	 */
-	async loadQueue() {
-		await PIXI.Assets.load(this._queue);
-		this._queue.length = 0;
-	}
+  /**
+   * Add a sound asset to the queue of sounds to load using Howler.js
+   *
+   * @param {*} sound - sound asset we need to load
+   */
+  addSoundToQueue(key, src) {
+    this._soundQueue[key] = src;
+  }
+
+  /**
+   * lead all assets added to the queue and reset the to the queue
+   *
+   * @async
+   */
+  async loadQueue() {
+    await PIXI.Assets.load(this._queue);
+    this._queue.length = 0;
+  }
+
+  /**
+   * Load all sound assets in the sound queue
+   */
+  loadSounds() {
+    for (const [key, src] of Object.entries(this._soundQueue)) {
+      soundManager.loadSound(key, src);
+    }
+    this._soundQueue = {};
+  }
 }
 
 export const assetLoader = new AssetLoader();
